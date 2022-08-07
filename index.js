b4d4efc675bf190ffeb3ab8e32e053776bfec46e
md tanvirul alam <tanvirulalam@protonmail.com>
2022-08-07 15:28:41 +0600

    blog site

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+  var num = 343244;
+
+
+
+                         num = toString(num)
+
+
+
+
+
+
+                                  console.log(typeof(num));
+
+
+
+
+
+
+
+
+
+
+                                  
